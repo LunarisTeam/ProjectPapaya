cfed1a7e28e14f209e993eb6b6e38c5f89d073f2
Alessandro Ricci <alessandro.ricci.rcc@gmail.com>
2025-03-07 16:27:55 +0100

    Initial commit

---
+++ ProjectPapaya/ProjectPapaya/ProjectPapayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPapayaApp.swift
+//  ProjectPapaya
+//
+//  Created by Alessandro Ricci on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPapayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
